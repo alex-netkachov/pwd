c70ce1c47fe65865cc8b489f8be05fbf73af88ec
Alex Netkachov <alex.netkachov@gmail.com>
2025-01-22 12:22:14 +0000

    add pwd

---
+++ pwd/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace pwd.ViewModels;
+
+public class MainWindowViewModel
+   : ViewModelBase
+{
+#pragma warning disable CA1822 // Mark members as static
+   public string Greeting => "Welcome to pwd!";
+#pragma warning restore CA1822 // Mark members as static
+}
---
+++ pwd/ViewModels/ViewModelBase.cs
@@ -0,0 +1,6 @@
+﻿using ReactiveUI;
+
+namespace pwd.ViewModels;
+
+public class ViewModelBase
+   : ReactiveObject;
